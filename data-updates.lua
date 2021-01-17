9b4ea65088b0e1a13cc991391b565a9577eb7ae0
Entroper <entroper@gmail.com>
2021-01-16 23:11:50 -0500

    initial commit

---
+++ data-updates.lua
@@ -0,0 +1,3 @@
+data.raw.boiler["heat-exchanger"].target_temperature = 515
+data.raw.boiler["heat-exchanger"].energy_source.min_working_temperature = 515
+data.raw.generator["steam-turbine"].maximum_temperature = 515
