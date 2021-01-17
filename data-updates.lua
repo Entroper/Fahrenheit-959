db8b2c13e0ea24bffc9662cf8cbfb56db2552b15
Entroper <entroper@gmail.com>
2021-01-17 12:15:57 -0500

    make temperature configurable

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,3 @@
-data.raw.boiler["heat-exchanger"].target_temperature = 515
-data.raw.boiler["heat-exchanger"].energy_source.min_working_temperature = 515
-data.raw.generator["steam-turbine"].maximum_temperature = 515
+data.raw.boiler["heat-exchanger"].target_temperature = settings.startup["heat-exchanger-steam-temperature"].value
+data.raw.boiler["heat-exchanger"].energy_source.min_working_temperature = settings.startup["heat-exchanger-steam-temperature"].value
+data.raw.generator["steam-turbine"].maximum_temperature = settings.startup["heat-exchanger-steam-temperature"].value
---
+++ settings-updates.lua
@@ -0,0 +1,9 @@
+data:extend({
+	{
+		type = "double-setting",
+		name = "heat-exchanger-steam-temperature",
+		localised_name = "Heat Exchanger steam temperature (⁰C)",
+		setting_type = "startup",
+		default_value = 515
+	}
+})
